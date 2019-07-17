5ee4215cdd2fde70c3f8b257faaf15c497664a84
326487162 <Welcome@661>
2019-07-17 14:54:20 -0400

    Add Jep example

--- Scala/main/scala/com/hibiup/examples/Example_Jep_CallToPython.scala
+++ Scala/main/scala/com/hibiup/examples/Example_Jep_CallToPython.scala
@@ -21,16 +21,37 @@ object Example_Jep_CallToPython extends App{
     val logger = Logger(this.getClass)
 
     def pythonGreeting[T](name:String)(implicit jep:Try[Jep]): Try[T] = jep.map(j => {
-        j.getValue(s"""JepService.sayHello("$name")""").asInstanceOf[T]
+        /**
+          * 2) import python module. 返回 boolean
+          * */
+        j.eval("""from Example_Jep_CallToPython import JepService""")
+
+        /**
+          * 3) 执行并获得返回值
+          * */
+        val result = j.getValue(s"""JepService.sayHello("$name")""").asInstanceOf[T]
+
+        /**
+          * 4) 关闭 Python
+          * */
+        j.close()
+
+        result
     })
 
-    /** new Jep() 会抛出一个不可 Try 异常: UnsatisfiedLinkError 所以不得不以传统方式捕获.*/
+    /**
+      * 1) 初始化 Jep 引擎
+      * */
     pythonGreeting[String]("""John""")(Try {
+        /**
+          * new Jep() 会抛出一个不可 Try 异常: UnsatisfiedLinkError 所以不得不以传统方式捕获.
+          */
         try new Jep() catch {
             case e: UnsatisfiedLinkError => throw new Throwable(e)
             case t:Throwable => throw t
         }
     }) match {
+        /** 5) 处理结果 */
         case Failure(f) => logger.error(f.getMessage, f)
         case Success(s) => println(s)
     }
